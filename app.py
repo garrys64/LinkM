dc622b1d47a65d7b53d5b48e8daef05af8146885
garrys64 <garrys64@web.de>
2026-06-13 13:28:23 +0200

    1

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from processors.BaseProcessor import BaseProcessor
 from processors.TollCollectProcessor import TollCollectProcessor
 from processors.XmlProcessor import XmlProcessor
 from processors.WhatsappProcessor import WhatsappProcessor
-from processors.a_OffenePostenProcessor import OffenePostenProcessor
+from processors.OffenePostenProcessor import OffenePostenProcessor
 from processors.BwiProcessor import BwiProcessor
 
 
@@ -40,78 +40,96 @@ st.set_page_config(
     layout="centered"
 )
 
-st.title("📊 Mein Processors")
+#----------- 
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
 
-st.write("Select a processor and upload the files")
-
-
-# =========================================================
-# PROCESSOR SELECT
-# =========================================================
-
-selected_processor_name = st.selectbox(
-    "Processor",
-    list(processor_dict.keys())
-)
-
-processor = processor_dict[selected_processor_name]
-
-
-# =========================================================
-# DYNAMIC UI
-# =========================================================
-
-data = processor.render_ui()
-
-
-# =========================================================
-# PROCESS BUTTON
-# =========================================================
-
-if st.button("🚀 Execute"):
-
-    try:
-
-        # проверяем что все файлы загружены
-        missing = False
-
-        for key, value in data.items():
-
-            # selectbox/date/etc пропускаем
-            if isinstance(value, str):
-                continue
-
-            if value is None:
-                missing = True
-
-        if missing:
-
-            st.error("Please fill in all the fields")
-
+if not st.session_state["logged_in"]:
+    password = st.text_input("Pass(1)", type="password")
+    if st.button("Ok"):
+    
+#В интерфейсе Streamlit Cloud: > Settings > Secrets > LIUDMILAS=....
+        if password == st.secrets.LIUDMILAS:
+            st.session_state["logged_in"] = True
+            st.rerun()
         else:
+            st.error("No")
 
-            with st.spinner("Processing..."):
+else:
+#-----------
 
-                result_files = processor.process(data)
+    st.title("📊 Mein Processors")
 
-            # создаём zip в памяти
-            zip_buffer = io.BytesIO()
+    st.write("Select a processor and upload the files")
 
-            with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
-                for file_path in result_files:
-                    zipf.write(file_path, arcname=os.path.basename(file_path))
-                    os.remove(file_path)
 
-            st.success("Done!")
-            st.balloons()
-            
-            st.download_button(
-                label="📥 Download all results",
-                data=zip_buffer,
-                file_name="results.zip",
-                mime="application/zip"
-            )
-            
-    except Exception as e:
+    # =========================================================
+    # PROCESSOR SELECT
+    # =========================================================
 
-        st.error(f"Ошибка: {e}")
+    selected_processor_name = st.selectbox(
+        "Processor",
+        list(processor_dict.keys())
+    )
+
+    processor = processor_dict[selected_processor_name]
+
+
+    # =========================================================
+    # DYNAMIC UI
+    # =========================================================
+
+    data = processor.render_ui()
+
+
+    # =========================================================
+    # PROCESS BUTTON
+    # =========================================================
+
+    if st.button("🚀 Execute"):
+
+        try:
+
+            # проверяем что все файлы загружены
+            missing = False
+
+            for key, value in data.items():
+
+                # selectbox/date/etc пропускаем
+                if isinstance(value, str):
+                    continue
+
+                if value is None:
+                    missing = True
+
+            if missing:
+
+                st.error("Please fill in all the fields")
+
+            else:
+
+                with st.spinner("Processing..."):
+
+                    result_files = processor.process(data)
+
+                # создаём zip в памяти
+                zip_buffer = io.BytesIO()
+
+                with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
+                    for file_path in result_files:
+                        zipf.write(file_path, arcname=os.path.basename(file_path))
+                        os.remove(file_path)
+
+                st.success("Done!")
+                st.balloons()
+                
+                st.download_button(
+                    label="📥 Download all results",
+                    data=zip_buffer,
+                    file_name="results.zip",
+                    mime="application/zip"
+                )
+                
+        except Exception as e:
+
+            st.error(f"Ошибка: {e}")
