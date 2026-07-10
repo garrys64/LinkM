827b10328da2f568e605a940429b3c49a39ca7c4
garrys64 <garrys64@web.de>
2026-07-10 16:28:17 +0200

    Update pdf_Samlung_new.py

--- processors/pdf_Samlung_new.py
+++ processors/pdf_Samlung_new.py
@@ -31,17 +31,20 @@ class pdf_Samlung_new(BaseProcessor):
         Anhangs = data["Anhangs"]
         
         #combined_pdf = pikepdf.Pdf.new()
+        st.write("1")
         with pikepdf.open(ZUGFeRD) as src_pdf:
+            st.write("2")
             #combined_pdf.pages.extend(src_pdf.pages)
             for file in Anhangs:
                 # Открываем каждый файл в байтовом буфере
                 with pikepdf.open(file) as src_pdf2:
                     # Добавляем все страницы из текущего файла в объединённый
                     src_pdf.pages.extend(src_pdf2.pages)
-
+        st.write("3")
         # Сохраняем результат в BytesIO, чтобы отдать через st.download_button
         buffer = BytesIO()
         src_pdf.save(buffer)
+        st.write("4")
         buffer.seek(0)
         data = {"df": buffer,"filename":  f"mitAnhang_{ZUGFeRD.name}", "mime": "application/pdf"}
         
