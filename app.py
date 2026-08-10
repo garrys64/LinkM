c7ae4b80e8fed22651256eeacfb2205a25a140c5
garrys64 <garrys64@web.de>
2026-08-10 15:49:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,21 @@ st.set_page_config(
 )
 
 #----------- 
-if True:
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+
+if not st.session_state["logged_in"]:
+    password = st.text_input("Pass(1)", type="password")
+    if st.button("Ok"):
+    
+#В интерфейсе Streamlit Cloud: > Settings > Secrets > LIUDMILAS=....
+        if password == st.secrets.LIUDMILAS:
+            st.session_state["logged_in"] = True
+            st.rerun()
+        else:
+            st.error("No")
+
+else:
 #-----------
 
     st.sidebar.header("📊 Mein Processors")
